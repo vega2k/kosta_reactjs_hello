d6f6e4dd01a1841591a9566708eb31d84ce0acea
vega2k <vega2k@naver.com>
2021-04-26 17:32:34 +0900

    setState hook 추가

--- src/components/MyComponentFunc.js
+++ src/components/MyComponentFunc.js
@@ -3,13 +3,30 @@ import React, { useState } from 'react';
 export const MyComponentFunc = ({name, children}) => {
     const [number, setNumber] = useState(0);
 
+    //useState를 여러개를 사용하는 경우
+    const [inputs, setInputs] = useState({
+        message:'',
+        username:''
+    });
+    const {message, username} = inputs;
+
+    const handleChange = (e) => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+        });
+    };
+    
     return (
         <div>
             <h5>부모 컴포넌트가 보내준 값 : {name}</h5>
             Children : {children}
             <p>Number 값 : {number}</p>
             <button onClick={() => setNumber(number + 1)}>증가</button>
-            <button onClick={() => setNumber(number - 1)}>감소</button>
+            <button onClick={() => setNumber(number - 1)}>감소</button> <br/>
+            Message : <input type="text" name="message" value={message} onChange={handleChange}/><br/>
+            Username : <input type="text" name="username" value={username} onChange={handleChange}/>
+
         </div>
     );
 };
