6c4139b37cee5fc88c838f8098eb948e89de404f
vega2k <vega2k@naver.com>
2021-04-26 17:22:22 +0900

    setState hook 추가

--- src/components/MyComponentFunc.js
+++ src/components/MyComponentFunc.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const MyComponentFunc = ({name, children}) => {
+    const [number, setNumber] = useState(0);
+
     return (
         <div>
             <h5>부모 컴포넌트가 보내준 값 : {name}</h5>
             Children : {children}
+            <p>Number 값 : {number}</p>
+            <button onClick={() => setNumber(number + 1)}>증가</button>
+            <button onClick={() => setNumber(number - 1)}>감소</button>
         </div>
     );
 };
